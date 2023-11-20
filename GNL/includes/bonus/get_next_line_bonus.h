763ca3b4d2010ef1f2e541cd2e55670c528ead50
cnatanae <cnatanae@student.42sp.org.br>
2023-11-20 18:42:58 -0300

    Changes to the GNL-Tester program and addition of more tests. 	modified:   GNL/Makefile # Added tag -g3 to compiling line 	modified:   GNL/bin/tester_gnl # Bin of actual version of GNL 	new file:   GNL/includes/texts/aleatory_lines.txt # New file of tests 	modified:   GNL/includes/texts/half_marge_top.txt # Change line of file 	modified:   GNL/src/tester_gnl.c # More tests add in the Tester

---
+++ GNL/includes/bonus/get_next_line_bonus.h
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 12:53:18 by cnatanae          #+#    #+#             */
+/*   Updated: 2023/11/18 07:48:50 by cnatanae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+/**@brief Struct to store a character and a pointer to the next character.
+ * 
+ * @param character for the character.
+ * @param *next the pointer to the next node.
+*/
+typedef struct s_char
+{
+	char			character;
+	struct s_char	*next;
+}					t_char;
+
+/**@brief Struct to store a variable and a pointer to the next node.
+ * 
+ * @param fd The file descriptor.
+ * @param pos The position of the buffer.
+ * @param read The number of bytes read.
+ * @param len The length of the struct s_list.
+ * @param buffer The buffer.
+ * @param *string The pointer to the next node.
+*/
+typedef struct s_list
+{
+	int		fd;
+	int		pos;
+	int		read;
+	int		len;
+	int		exist_n;
+	char	*buffer;
+	t_char	*string;
+}			t_list;
+
+/**@brief Function to get the next line.
+ * 
+ * @param fd The file descriptor.
+ * @return char* The next line.
+*/
+char	*get_next_line(int fd);
+
+/**@brief Function to clear the linked list.
+ * 
+ * @param *str The pointer to the linked list.
+ * @return char* The pointer to NULL.
+*/
+void	*free_all(t_char **str);
+
+/**@brief Function to create a new node and add character to it.
+ * 
+ * @param *str The character to be added.
+ * @return t_char* The pointer to the new node.
+*/
+t_char	*ft_node(char c);
+
+/**@brief Function to get the size of the linked list.
+ * 
+ * @param *lst The pointer to the linked list.
+ * @param *struc The pointer to the struct t_list.
+ * @return int The size of the linked list.
+*/
+int		ft_lstsize(t_char *lst, t_list *struc);
+
+/**@brief Function to find the character '\n' in the linked list.
+ * 
+ * @param *struc The pointer to the struct t_list.
+ * @return int 1 if '\n' is found, 0 if not.
+*/
+int		ft_find_n(t_list *struc);
+
+/**@brief Function to build the string.
+ * 
+ * @param *struc The pointer to the struct t_list.
+ * @return char* The pointer to the string.
+*/
+char	*ft_build_string(t_list *struc);
+
+/**@brief Function to add the buffer to the linked list.
+ * 
+ * @param *struc The pointer to the struct t_list.
+ * @return char* The pointer to the string.
+*/
+char	*ft_add_lk(t_list *struc);
+
+/**@brief Function to add a new node to the end of the linked list.
+ * 
+ * @param *struc The pointer to the struct t_list.
+ * @param *end The pointer to the last node of the linked list.
+ * @param c The character to be added.
+ * @return t_char* The pointer to the new node.
+*/
+t_char	*add_node_to_end(t_list *struc, t_char *end, char c);
+
+#endif
--- GNL/src/tester_gnl.c
+++ GNL/src/tester_gnl.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 09:19:48 by cnatanae          #+#    #+#             */
-/*   Updated: 2023/11/18 13:44:21 by cnatanae         ###   ########.fr       */
+/*   Updated: 2023/11/20 14:08:50 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 MU_TEST(test_gnl_passing_empy_file)
 {
 	int fd;
-	char *expeted_result;
+	char *expected_result;
 	char *result;
 	fd = open ("includes/texts/5-empty.txt", O_RDONLY);
 	if (fd == -1)
@@ -24,17 +24,18 @@ MU_TEST(test_gnl_passing_empy_file)
 		exit(1);
 	}
 	result = get_next_line(fd);
-	expeted_result = NULL;
+	expected_result = NULL;
+	mu_assert_string_eq(expected_result, result);
 	close(fd);
-	mu_assert_string_eq(expeted_result, result);
 	free(result);
 }
 
 MU_TEST(test_gnl_passing_one_char)
 {
 	int fd;
-	char expeted_result[3] = "z\n\0";
+	char expected_result[3] = "z\n\0";
 	char *result;
+	
 	fd = open ("includes/texts/7-onechar.txt", O_RDONLY);
 	if (fd == -1)
 	{
@@ -43,40 +44,262 @@ MU_TEST(test_gnl_passing_one_char)
 	}
 	result = get_next_line(fd);
 	close(fd);
-	mu_assert_string_eq(expeted_result, result);
+	mu_assert_string_eq(expected_result, result);
 	free(result);
 }
 
 MU_TEST(test_gnl_passing_bigben)
 {
-	char	*expeted_result;
+	char	*expected_result;
 	char	*result;
 	char	line[1024];
-	int		i;
 	int		fd;
 	FILE	*file;
 
-	i = 0;
 	file = fopen("includes/texts/12-bigben.txt", "r");
 	fd = open ("includes/texts/12-bigben.txt", O_RDONLY);
 	if ( !file ||fd == -1)
 	{
 		perror("Error opening file test 3");
+		close(fd);
+		fclose(file);
 		exit(1);
 	}
-	while(i++ <= 70)
+	while(1)
 	{
 		result = get_next_line(fd);
-		expeted_result = fgets(line, sizeof(line), file);
-		if (expeted_result == NULL && result == NULL)
+		expected_result = fgets(line, sizeof(line), file);
+		if (expected_result == NULL && result == NULL)
+		{
 			mu_assert_int_eq(1, 1);
-		else if (expeted_result == NULL && result != NULL)
+			break;
+		}
+		else if (expected_result == NULL && result != NULL)
+		{
 			mu_assert_int_eq(1, 2);
-		else if(strcmp(expeted_result, result) != 0)
+			free(result);
+			break;
+		}
+		else if (strcmp(expected_result, result) != 0)
+		{
 			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
 		free(result);
 	}
-	mu_assert_int_eq(1, 1);
+	close(fd);
+	fclose(file);
+}
+
+MU_TEST(test_gnl_passing_marge_top)
+{
+	char	*expected_result;
+	char	*result;
+	char	line[1024];
+	int		fd;
+	FILE	*file;
+
+	file = fopen("includes/texts/half_marge_top.txt", "r");
+	fd = open ("includes/texts/half_marge_top.txt", O_RDONLY);
+	if ( !file ||fd == -1)
+	{
+		perror("Error opening file test 4");
+		exit(1);
+	}
+	while(1)
+	{
+		result = get_next_line(fd);
+		expected_result = fgets(line, sizeof(line), file);
+		if (expected_result == NULL && result == NULL)
+		{
+			mu_assert_int_eq(1, 1);
+			break;
+		}
+		else if (expected_result == NULL && result != NULL)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		else if (strcmp(expected_result, result) != 0)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		free(result);
+	}
+	close(fd);
+	fclose(file);
+}
+
+MU_TEST(test_gnl_passing_marge_bottom)
+{
+	char	*expected_result;
+	char	*result;
+	char	line[1024];
+	int		fd;
+	FILE	*file;
+
+	file = fopen("includes/texts/half_marge_bottom.txt", "r");
+	fd = open ("includes/texts/half_marge_bottom.txt", O_RDONLY);
+	if ( !file ||fd == -1)
+	{
+		perror("Error opening file test 5");
+		exit(1);
+	}
+	while(1)
+	{
+		result = get_next_line(fd);
+		expected_result = fgets(line, sizeof(line), file);
+		if (expected_result == NULL && result == NULL)
+		{
+			mu_assert_int_eq(1, 1);
+			break;
+		}
+		else if (expected_result == NULL && result != NULL)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		else if (strcmp(expected_result, result) != 0)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		free(result);
+	}
+	close(fd);
+	fclose(file);
+}
+
+MU_TEST(test_gnl_passing_aleatory_lines)
+{
+	int		fd;
+	char	*expected_result;
+	char	*result;
+	char	line[1024];
+	int		i;
+	FILE	*file;
+
+	i = 0;
+	file = fopen("includes/texts/aleatory_lines.txt", "r");
+	fd = open("includes/texts/aleatory_lines.txt", O_RDONLY);
+	if ( !file ||fd == -1)
+	{
+		perror("Error opening file test 6");
+		exit(1);
+	}
+	while(1)
+	{
+		result = get_next_line(fd);
+		expected_result = fgets(line, sizeof(line), file);
+		if (expected_result == NULL && result == NULL)
+		{
+			mu_assert_int_eq(1, 1);
+			break;
+		}
+		else if (expected_result == NULL && result != NULL)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		else if (strcmp(expected_result, result) != 0)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		free(result);
+	}
+	close(fd);
+	fclose(file);
+}
+
+MU_TEST(test_gnl_passing_gnl_file)
+{
+	int		fd;
+	char	*expected_result;
+	char	*result;
+	char	line[1024];
+	FILE	*file;
+
+	file = fopen("src/minunit.h", "r");
+	fd = open("src/minunit.h", O_RDONLY);
+	if ( !file ||fd == -1)
+	{
+		perror("Error opening file test 7");
+		exit(1);
+	}
+	while(1)
+	{
+		result = get_next_line(fd);
+		expected_result = fgets(line, sizeof(line), file);
+		if (expected_result == NULL && result == NULL)
+		{
+			mu_assert_int_eq(1, 1);
+			break;
+		}
+		else if (expected_result == NULL && result != NULL)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		else if (strcmp(expected_result, result) != 0)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		free(result);
+	}
+	close(fd);
+	fclose(file);
+}
+
+MU_TEST(test_gnl_passing_alphabet_file)
+{
+	int fd;
+	char *expected_result;
+	char *result;
+	char line[1024];
+	FILE *file;
+
+	file = fopen("includes/texts/alphabet.txt", "r");
+	fd = open("includes/texts/alphabet.txt", O_RDONLY);
+	if (!file || fd == -1)
+	{
+		perror("Error opening alphabet.txt");
+		exit(1);
+	}
+	while (1)
+	{
+		result = get_next_line(fd);
+		expected_result = fgets(line, sizeof(line), file);
+		if (expected_result == NULL && result == NULL)
+		{
+			mu_assert_int_eq(1, 1);
+			break;
+		}
+		else if (expected_result == NULL && result != NULL)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		else if (strcmp(expected_result, result) != 0)
+		{
+			mu_assert_int_eq(1, 2);
+			free(result);
+			break;
+		}
+		free(result);
+	}
 	close(fd);
 	fclose(file);
 }
@@ -86,6 +309,11 @@ MU_TEST_SUITE(Get_next_line_tester)
 	MU_RUN_TEST(test_gnl_passing_empy_file);
 	MU_RUN_TEST(test_gnl_passing_one_char);
 	MU_RUN_TEST(test_gnl_passing_bigben);
+	MU_RUN_TEST(test_gnl_passing_marge_top);
+	MU_RUN_TEST(test_gnl_passing_marge_bottom);
+	MU_RUN_TEST(test_gnl_passing_aleatory_lines);
+	MU_RUN_TEST(test_gnl_passing_gnl_file);
+	MU_RUN_TEST(test_gnl_passing_alphabet_file);
 }
 
 int	main(void)
